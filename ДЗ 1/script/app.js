fc2fd645dbda515c77e06f6de7852cbe93a28270
1blago1 <104318779+1blago1@users.noreply.github.com>
2022-05-10 21:08:14 +0300

    Add files via upload

---
+++ ДЗ 1/script/app.js
@@ -0,0 +1,46 @@
+// let mounth = prompt("Сколько месяцев в году?");
+// if (mounth == "12") {
+//   alert("Верно)");
+// } else if (mounth > "12") {
+//   alert("не верно!");
+// } else if (mounth < "12") {
+//   alert("попробуйте снова!");
+// }
+
+// let numberOne = +prompt("Введите первое число");
+// let numberTwo = +prompt("Введите второе число");
+// let numberTree = +prompt("Введите третье число");
+// let sum = alert(numberOne + numberTwo + numberTree);
+
+// let nameUser = prompt("Как вас зовут?");
+// let age = prompt("Сколько вам лет?");
+// if (age >= "18") {
+//   alert(`Добро пожаловать!${nameUser}`);
+// } else if (age < "18") {
+//   alert(`Доступ закрыт!${nameUser}`);
+// }
+
+// let numOne = +prompt("enter num one");
+// let numTwo = +prompt("enter num two");
+// let plus = numOne + numTwo;
+// let minus = numOne - numTwo;
+// let div = numOne / numTwo;
+// let mult = numOne * numTwo;
+// if (numOne && numTwo) {
+//   let res = prompt("enter operation");
+//   if (res === "*") {
+//     alert(mult);
+//   }
+//   if (res === "/") {
+//     alert(div);
+//   }
+//   if (res === "-") {
+//     alert(minus);
+//   }
+//   if (res === "+") {
+//     alert(plus);
+//   }
+// }
+
+
+
